2b3f9ae680ec5de33ef54e835e1ae9ed682cdf72
tejaabp <tejaabp@gmail.com>
2024-10-02 11:13:37 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import FullNameDisplay from './components/FullNameDisplay';
+
+function App() {
+  return (
+    <div className="App">
+     <FullNameDisplay />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/FullNameDisplay.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+import './styles.css'
+
+const FullNameDisplay = () => {
+    const [name, setName] = useState({ firstname: '', lastname: '' })
+    const [fullname, setFullname] = useState('')
+    const handleChange = (e) => {
+        setName({
+            ...name,
+            [e.target.id]: e.target.value
+        })
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        setFullname(`${name.firstname} ${name.lastname}`)
+    }
+    return (
+        <div className='main-div'>
+            <h2>
+                Full Name Display
+            </h2>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor='firstname'>First Name:{' '} <input required onChange={handleChange} id='firstname' /> </label>
+                </div>
+                <div>
+                    <label htmlFor='lastname'>Last Name: <input required onChange={handleChange} id='lastname' /> </label>
+                </div>
+                <div><input type='submit' /></div>
+            </form>
+            <br />
+            <p>Full Name: {fullname}</p>
+        </div>
+    )
+}
+
+export default FullNameDisplay
